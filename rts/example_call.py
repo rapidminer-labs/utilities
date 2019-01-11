df596e436a0ac782320ba64953ba744b6b9fbf91
MartinSchmitz <mschmitz@rapidminer.com>
2019-01-11 17:09:52 +0100

    Quick fix on idents

--- rts/example_call.py
+++ rts/example_call.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import requests
 import json
+
 #
 # Specify the full url to the service here. It's usually
 # $URL/services/$endpointname/$wsname/
 #
 url = "http://127.0.0.1:8090/services/repository/webservice_v2/"
+
 
 #
 # This function takes a pandas data frame, sents it to the RTS and gets back a scored version of it.
@@ -27,9 +29,9 @@ def call_rm_process(df, url):
 # Quick example main, which sents the test.csv examples overø
 #
 if __name__ == '__main__':
-     df = pd.read_csv("test.csv")
-     print(df)
-	 print("Sending it to RTS")
-     processed_df = call_rm_process(df,url)
-	 print("This is the processed df:")
-     print(processed_df)
+    df = pd.read_csv("test.csv")
+    print(df)
+    print("Sending it to RTS")
+    processed_df = call_rm_process(df, url)
+    print("This is the processed df:")
+    print(processed_df)
